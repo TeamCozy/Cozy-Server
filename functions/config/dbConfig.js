a9c183c8d5af708979f6e1757b29cb3a92915b38
namjiyun <namjiyun3275@gmail.com>
2022-02-12 19:16:29 +0900

    [ADD] DB setting

---
+++ functions/config/dbConfig.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dotenv_1 = require('dotenv');
+
+dotenv_1.config();
+
+module.exports = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PASSWORD,
+};
