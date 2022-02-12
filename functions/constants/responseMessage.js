49c23929ddfa7de2dea2617b45ffc8c3bd91903e
namjiyun <namjiyun3275@gmail.com>
2022-02-12 18:49:29 +0900

    [ADD] firebase 보일러플레이트, express관련 코드 추가

---
+++ functions/constants/responseMessage.js
@@ -0,0 +1,53 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+
+  // 회원가입
+  CREATED_USER: '회원 가입 성공',
+  DELETE_USER: '회원 탈퇴 성공',
+  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+  PASSWORD_LENGTH_SHORT: '비밀번호가 8자리 이하입니다.',
+  PASSWORD_IS_NOT_CORRECT: '비밀번호가 일치하지 않습니다.',
+
+  // 로그인
+  LOGIN_SUCCESS: '로그인 성공',
+  LOGIN_FAIL: '로그인 실패',
+  NO_USER: '존재하지 않는 회원입니다.',
+  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+  INVALID_EMAIL: '이메일이 유효하지 않습니다.',
+
+  // 프로필 조회
+  READ_PROFILE_SUCCESS: '프로필 조회 성공',
+
+  // 유저 관련
+  READ_USER_SUCCESS: '유저 조회 성공',
+
+  // 친구신청
+  FAIL_ADD_FRIEND: '친구신청 실패',
+  SUCCESS_ADD_FRIEND: '친구신청 성공',
+  ACCEPT_FRIEND: '친구신청 수락',
+  REFUSE_FRIEND: '친구신청 거절',
+  EXISTS_FRIEND: '이미 친구입니다.',
+  INTERNAL_SERVER_ERROR: '서버 에러',
+
+  //초대장
+  INVITATION_SUCCESS: '초대 성공',
+  READ_ALL_INVITATION_SUCCESS: '초대장 목록 조회 성공',
+  NO_INVITATION: '초대장이 존재하지 않습니다.',
+  ALREADY_CONFIRM: '이미 확정된 약속입니다.',
+  INVITATION_CONFIRM_SUCCESS: '약속 확정 성공',
+  INVITATION_CANCLE_SUCCESS: '약속 취소 성공',
+  INVITATION_FAIL: '초대장 생성 실패',
+  READ_ALL_INVITATION_FAIL: '초대장 목록 조회 실패',
+  READ_INVITATION_SUCCESS: '초대장 조회 성공',
+  READ_INVITATION_FAIL: '초대장 조회 실패',
+  INVITATION_CONFIRM_FAIL: '약속 확정 실패',
+  INVITATION_CANCLE_FAIL: '약속 취소 실패',
+  NO_INVITATION_DATE: '약속 날짜 조회 실패',
+
+  //초대장 응답
+  INVITATION_RESPONSE_SUCCESS: '초대장 응답 성공',
+  INVITATION_REJECT_SUCCESS: '초대장 거절 성공',
+  INVITATION_RESPONSE_FAIL: '초데징 응답 실패',
+  INVITATION_REJECT_FAIL: '초대장 거절 실패',
+};
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
-const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./cozy-57e78-firebase-adminsdk-h4pqw-d2ac2e2a7c");
+const dotenv = require("dotenv");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require("./api"),
+};
