6d7c67237553ba3ae5f64188b651cb46a4a7b749
kmebin <blowhuiee@gmail.com>
2022-02-14 23:11:19 +0900

    [ADD] Route 세팅

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// '/user' 이하의 경로로 들어온 요청은 모두 user 폴더 안에서 처리
-//router.use('/user', require('./user'));
+router.use('/auth', require('./auth'));
+router.use('/user', require('./user'));
+router.use('/clinic', require('./clinic'));
+router.use('/review', require('./review'));
 
 module.exports = router;
