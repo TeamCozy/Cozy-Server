fec7a6eca58c8922f26e24ad835d4fb3454bcd0b
kmebin <blowhuiee@gmail.com>
2022-02-21 20:10:26 +0900

    [ADD] 특정 진료소 상세 조회 API

--- functions/api/routes/clinic/index.js
+++ functions/api/routes/clinic/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.get('/', require('./clinicAllGET'));
 router.get('/search', require('./clinicSearchGET'));
+router.get('/:clinicId', require('./clinicOneGET'));
 
 module.exports = router;
