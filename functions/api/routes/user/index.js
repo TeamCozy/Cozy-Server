3a726ad6cc0497ac963a8e5d1829cd1d889613f3
namjiyun <namjiyun3275@gmail.com>
2022-12-27 14:06:47 +0900

    [FEAT] user api 추가

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/info', require('./userInfoGET'));
-
+router.delete('/userDelete', require('./userDELETE'));
 module.exports = router;
